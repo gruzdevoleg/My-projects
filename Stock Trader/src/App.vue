cef61b6d579096c37fce481af3b6c5a38f980d55
Oleg Gruzdev <oleg.gruzdev.1982@gmail.com>
2020-03-25 17:59:39 +0200

    'stock_trader-app'

---
+++ Stock Trader/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filters & Mixins</h1>
+                <!-- Exercise 1) -->
+                <!-- Build a local Filter which reverses the Text it is applied on -->
+                <p> {{ message | reverseText }}</p>
+                
+                <!-- Exercise 2 -->
+                <!-- Build a global Filter which counts the length of a word and it appends it -->
+                <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+                 <p> {{ word | count-word-length }}</p>
+                <!-- Exercise 3 -->
+                <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
+                <input type="text" v-model="message">
+                <input type="text" v-model="word">
+                <p> {{ reverseText }}</p>
+                <p> {{ countWordLength }}</p>
+                <!-- Exercise 4 -->
+                <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    // import reverseMixin from './main.js';
+
+    //local mixin
+    let reverseMixin = {
+        created() {
+            return this.message.split('').reverse().join('');
+        }
+    };
+    
+    export default {
+        data() {
+            return {
+                message: '',
+                word: ''
+            }
+        },
+
+        filters: {
+            reverseText(text) {
+                return text.split('').reverse().join('');
+            }
+        },
+
+        computed: {
+            
+
+            countWordLength() {
+                return `${this.word} (${this.word.length})`;
+            }
+        },
+
+        mixins: [reverseMixin],
+    }
+</script>
+
+<style>
+input {
+    display: block;
+    margin: 15px;
+}
+</style>
---
+++ Stock Trader/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router';
+import Vuelidate from 'vuelidate';
+
+Vue.use(Vuelidate);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+  });
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
