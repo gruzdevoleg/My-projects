71cdedd9a4b215a878a5c4fc7c90c6bfec675513
Oleg Gruzdev <oleg.gruzdev.1982@gmail.com>
2020-05-11 20:40:57 +0300

    test commit

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -22,9 +22,7 @@
                   :disabled="excessQuantity || +quantity <= 0 || !Number.isInteger(+quantity)"
                   >{{ excessQuantity ? 'Not enough stocks' : 'Sell'}}</button>
               </div>
-              <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a> -->
-          </div>
+           </div>
       </div>
     </div>
 </template>
